60b7952b9661a17574c7e01e001e6ede71c7903b
Xander <xvanoven@gmail.com>
2026-08-01 19:46:33 -0500

    Initial commit and Day One

---
+++ helloworld.py
@@ -0,0 +1,26 @@
+# Introduction
+# This is Day 1 of 30 to the 30 Days of Python Challenge.
+
+print("Hello, World!")
+
+print(2 + 3)    # addition(+)
+print(3 - 1)    # subtraction(-)
+print(2 * 3)    # multiplication(*)
+print(3 + 2)    # addition(+)
+print(3 - 2)    # subtraction(-)
+print(3 * 2)    # multiplication(*)
+print(3 / 2)    # division(/)
+print(3 ** 2)   # exponential(**)
+print(3 % 2)    # modulus(%)
+print(3 // 2)   # floor division operator(//)
+
+# Checking data types of the results of the operations
+
+print(type(10))                  # int
+print(type(3.14))                # float
+print(type(1 + 3j))              # complex
+print(type('xvanoven'))          # string
+print(type([1, 2, 3]))           # list
+print(type({'name':'xvanoven'})) # dictionary
+print(type({9.8, 3.14, 2.7}))    # set
+print(type((9.8, 3.14, 2.7)))    # tuple
