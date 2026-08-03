4e193eb4511a222f644939f95b67a5af750893e6
Xander <xvanoven@gmail.com>
2026-08-03 13:29:34 -0500

    Update strings.py

--- strings.py
+++ strings.py
@@ -44,3 +44,116 @@ print(company.swapcase())
 # 9. Cut(slice) out the first word of Coding For All String
 
 print(company[7:])
+
+# 10. Check if Coding For All string contains a word Coding using the method index, find or other methods.
+
+if 'Coding' in company:
+    print('Substring "Coding" found in company!')
+
+# 11. Replace the word coding in the string 'Coding For All' to Python.
+
+print(company.replace('coding', 'python'))
+
+# 12. Change "Python for Everyone" to "Python for All" using the replace method or other methods.
+
+str1 = 'Python for Everyone'.lower()
+print(str1.replace('all', 'everyone'))
+
+# 13. Split the string 'Coding For All' using space as the separator (split()) .
+
+str1 = 'Coding For All'
+print(str1.split())
+
+# 14. "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon" split the string at the comma.
+
+str1 = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
+print(str1.split(', '))
+
+# 15. What is the character at index 0 in the string Coding For All.
+
+str1 = 'Coding For All'
+print(str1[0])
+
+# 16. What is the last index of the string Coding For All.
+
+
+
+# 17. What character is at index 10 in "Coding For All" string.
+
+
+
+# 18. Create an acronym or an abbreviation for the name 'Python For Everyone'.
+
+
+
+# 19. Create an acronym or an abbreviation for the name 'Coding For All'.
+
+
+
+# 20. Use index to determine the position of the first occurrence of C in Coding For All.
+
+
+
+# 21. Use index to determine the position of the first occurrence of F in Coding For All.
+
+
+
+# 22. Use rfind to determine the position of the last occurrence of l in Coding For All People.
+
+
+
+# 23. Use index or find to find the position of the first occurrence of the word 'because' in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
+
+
+
+# 24. Use rindex to find the position of the last occurrence of the word because in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
+
+
+
+# 25. Slice out the phrase 'because because because' in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
+
+
+
+# 26. Find the position of the first occurrence of the word 'because' in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
+
+
+
+# 27. Slice out the phrase 'because because because' in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
+
+
+
+# 28. Does 'Coding For All' start with a substring Coding?
+
+
+
+# 29. Does 'Coding For All' end with a substring coding?
+
+
+# 30. '   Coding For All      '  , remove the left and right trailing spaces in the given string.
+
+
+
+# 31. Which one of the following variables return True when we use the method isidentifier(): 30DaysOfPython, thirty_days_of_python
+
+
+
+# 32. The following list contains the names of some of python libraries: ['Django', 'Flask', 'Bottle', 'Pyramid', 'Falcon']. Join the list with a hash with space string.
+
+
+
+# 33. Use the new line escape sequence to separate the following sentences.
+
+
+
+# 34. Use a tab escape sequence to write the following lines: 
+
+
+
+# 35. Use the string formatting method to display the following:
+
+
+
+# 36. Make the following using string formatting methods: 
+
+
+
